867f3a243d13ccd80dac83b7fc4eb05b6747bf43
KJIHYEON <64995083+KJIHYEON@users.noreply.github.com>
2021-01-07 12:57:31 +0900

    Intent_practice(2)

--- app/src/main/java/com/example/intentpractice/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_a.setOnClickListener {
+            val intentActivity = Intent(this@MainActivity, SubActivity::class.java)
+            //다음 화면으로 이동하기 위한 인텐트 작성
+            intentActivity.putExtra("msg", hello.text.toString())
+            //HelloWorld라는 텍스트 값을 담은 뒤 msg 라는 키로 잠궜다
+            startActivity(intentActivity)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/intentpractice/SubActivity.kt
@@ -0,0 +1,19 @@
+package com.example.intentpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        if (intent.hasExtra("msg"))
+        {
+            hello2.text = intent.getStringExtra("msg")
+            //서브 액티비티에 존재하는 텍스트뷰에다가 Helloworld 가 넘겨져 옴
+        }
+
+    }
+}
